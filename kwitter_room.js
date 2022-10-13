613370fc087a719ce859c3515b35c77c877b4f56
Squid-coder1 <90101725+Squid-coder1@users.noreply.github.com>
2022-10-13 18:45:40 -0400

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -44,3 +44,9 @@ function redirectToRoomName(name) {
       localStorafe.setItem("room_name", name);
       window.location="kwitter_page.html";
 }
+ 
+function logout() {
+      localStorage.removeItem("user_name");
+      localStorage.removeItem("room_name");
+      window.location ="kwitter.html";
+}
